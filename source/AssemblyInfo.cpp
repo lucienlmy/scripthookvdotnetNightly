64a75e5d9ca10a6924b7d9e0d112243b03fa6bdf
lucienlmy <101241027+lucienlmy@users.noreply.github.com>
2024-12-23 21:55:12 +0800

    更新 AssemblyInfo.cpp加入版本号方法

--- source/AssemblyInfo.cpp
+++ source/AssemblyInfo.cpp
@@ -1,10 +1,12 @@
+#define SHVDN_VERSION "2.10.10"
+
 using namespace System::Reflection;
 
-[assembly:AssemblyTitleAttribute("Community Script Hook V .NET")];
-[assembly:AssemblyDescriptionAttribute("An ASI plugin for Grand Theft Auto V, which allows running scripts written in any .NET language in-game.")];
-[assembly:AssemblyCopyrightAttribute("Copyright © 2015 crosire")];
+[assembly:AssemblyTitleAttribute("Community Script Hook V .NET")] ;
+[assembly:AssemblyDescriptionAttribute("An ASI plugin for Grand Theft Auto V, which allows running scripts written in any .NET language in-game.")] ;
+[assembly:AssemblyCopyrightAttribute("Copyright © 2015 crosire")] ;
 
 #ifdef SHVDN_VERSION
 [assembly:AssemblyVersionAttribute(SHVDN_VERSION)];
-[assembly:AssemblyFileVersionAttribute(SHVDN_VERSION)];
+[assembly:AssemblyFileVersionAttribute(SHVDN_VERSION)] ;
 #endif
